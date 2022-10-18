3fecc79d6d1ad58b61c2210b35680479e77b1916
JangKroed <didlsdydgh@gmail.com>
2022-10-18 21:57:46 +0900

    comments 주석

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use('/', require('./routes/users.routes'));
 app.use('/posts', require('./routes/posts.routes'));
-app.use('/comments', require('./routes/comments.routes'));
+// app.use('/comments', require('./routes/comments.routes'));
 
 http.listen(port, () => {
     console.log(`Start listen Server: ${port}`);
