d60534bda2b5a8bcc28cf65b8a1e1476b96f89fd
JangKroed <didlsdydgh@gmail.com>
2022-10-18 21:58:00 +0900

    미들웨어 오타 수정

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middlewares/authMiddleware')
+const authMiddleware = require('../middlewares/authMiddlewars')
 
 const PostsController = require('../controllers/posts.controller');
 const postsController = new PostsController();
